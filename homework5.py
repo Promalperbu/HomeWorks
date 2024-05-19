cfe799e2f8f414503e8df71614739cf377b253bf
Promalperbu <promalperbu@gmail.com>
2024-05-19 18:13:50 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,14 @@
+# Работа со списками
+my_list = ['apple','peach','coconut','orange','papaya','pineapple']
+print(my_list)
+print(my_list[0],my_list[-1])
+print(my_list[2:4])
+my_list [2] = 'banana'
+print(my_list)
+# Работа со словарями
+my_dict = {'pineapple': 'ананас', 'papaya':'папая', 'orange':'апельсин', 'coconut':'кокос','peach':'персик','apple':'яблоко'}
+print(my_dict)
+print(my_dict['papaya'])
+my_dict['pineapple']='ananas'
+my_dict['mango']='манго'
+print(my_dict)
