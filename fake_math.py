a22dfe6fe55e87bdd06bbcb515aefddaff222cd8
Promalperbu <promalperbu@gmail.com>
2024-06-25 16:42:45 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,11 @@
+def divide(first, second):
+     if second == 0:
+        return  'Ошибка'
+
+     else:
+        divide = first/second
+        return divide
+
+
+
+
---
+++ true_math.py
@@ -0,0 +1,11 @@
+def divide(first, second):
+    if second == 0:
+        from math import inf
+        divide = inf
+        return inf
+    else:
+        divide = first / second
+        return divide
+
+
+
