898918010b410ff87ad3c2618e87e69cc8e5f105
Promalperbu <promalperbu@gmail.com>
2024-08-06 19:17:34 +0300

    Add files via upload

---
+++ module_6_1.py
@@ -0,0 +1,60 @@
+
+class Animal:
+
+    """ атрибуты alive = True(живой) и fed = False(накормленный), name - индивидуальное название каждого животного.
+    """
+
+    alive = True
+    fed = False
+
+    def __init__(self, name):
+        self.name = name
+
+    def eat(self, food):
+        if food.edible:
+            self.fed = True
+            print (f"{self.name} съел {food.name}")
+
+        else:
+            self.alive = False
+            print (f"{self.name} не стал есть {food.name}")
+
+
+class Plant:
+    """атрибут edible = False(съедобность), name - индивидуальное название каждого растения
+"""
+    edible = False
+    def __init__(self, name):
+        self.name = name
+
+class Mammal(Animal):
+   pass
+
+class Predator(Animal):
+   pass
+
+class Flower (Plant):
+    pass
+
+
+class Fruit (Plant):
+    edible = True
+
+
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
+
+# Что произошло: Хищник попытался съесть цветок и погиб, млекопитающее съело фрукт и насытилось.
